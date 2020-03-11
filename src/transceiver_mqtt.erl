82b1c406f093b39e6a4b2e14a68fc4dddc81e935
Ilya <ilyaposlayko@gmail.com>
2020-03-11 20:01:21 +0200

    little fixs

--- src/transceiver_mqtt.erl
+++ src/transceiver_mqtt.erl
@@ -13,6 +13,7 @@
 
 -define(TOPIC_SUB_REGISTER, application:get_env(transceiver, topic_sub_register)).
 -define(TOPIC_SUB_EVENT, application:get_env(transceiver, topic_sub_event)).
+-define(TOPIC_SUB_CON, application:get_env(transceiver, topic_sub_con)).
 -define(START_OPT_MQTT, application:get_env(transceiver, start_options_mqtt)).
 -define(QOS, application:get_env(transceiver, qos)).
 
@@ -38,14 +39,16 @@ handle_cast(_Request, State) ->
 handle_info(first_subscribe, _State) ->
     {ok, TopicSubRegister} = ?TOPIC_SUB_REGISTER,
     {ok, TopicSubEvent} = ?TOPIC_SUB_EVENT,
-    Topics = [TopicSubRegister, TopicSubEvent],
+    {ok, TopicSubCon} = ?TOPIC_SUB_CON,
+    Topics = [TopicSubRegister, TopicSubEvent, TopicSubCon],
     NewState = subscribe_mqtt(Topics),
     {noreply, NewState};
 handle_info({publish, MsgMap}, State) ->
     Payload = maps:get(payload, MsgMap),
     MsgTerm = binary_to_term(Payload),
 
-    ok = io:format("~nMsgTerm: ~p~n", [MsgTerm]),
+    ok = io:format("~n~nMsgMap: ~p~n", [MsgMap]),
+    ok = io:format("~nMsgTerm: ~p~n~n", [MsgTerm]),
 
     {noreply, State};
 handle_info(Info, State) ->
@@ -55,10 +58,11 @@ handle_info(Info, State) ->
     {noreply, State}.
 
 subscribe_mqtt(Topics) ->
-    [TopicRegister, TopicEvent] = Topics,
+    [TopicRegister, TopicEvent, TopicCon] = Topics,
     {ok, StartOptions} = ?START_OPT_MQTT,
     {ok, ConnPid} = emqtt:start_link(StartOptions),
     {ok, _Props} = emqtt:connect(ConnPid),
     {ok, QoS} = ?QOS,
     {ok, _Props, _ReasonCodes} = emqtt:subscribe(ConnPid, {TopicRegister, QoS}),
-    {ok, _Props2, _ReasonCodes2} = emqtt:subscribe(ConnPid, {TopicEvent, QoS}).
+    {ok, _Props2, _ReasonCodes2} = emqtt:subscribe(ConnPid, {TopicEvent, QoS}),
+    {ok, _Props3, _ReasonCodes3} = emqtt:subscribe(ConnPid, {TopicCon, QoS}).
